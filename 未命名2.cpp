0f55863e4ea922afa0f5a7ed97f1f19de1b80591
PECompact <114324004+PECompact@users.noreply.github.com>
2022-11-09 17:18:38 +0800

    Add files via upload

---
+++ 未命名2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string>
+#include <ctype.h>
+
+using namespace std;
+
+int main()  {
+    double aaa;
+    scanf("%lf", &aaa);
+    printf("%.3f", aaa);
+
+}
